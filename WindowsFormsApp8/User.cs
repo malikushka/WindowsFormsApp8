8ebdfff37dd3eb77b7365ce3b66d087bac8bf086
маликушка <kahramanmalika@gmail.com>
2023-04-18 21:23:00 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp8/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp8
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DateofBirth { get; set; }     
+        public string Telephone { get; set;}
+        public string Direction { get; set; }
+        public string NumberWorks { get; set; }
+        public string FirstPublication { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public User()
+        { }
+        public User(string Login, string Password, string Email, string Role, string name, string lastName, string dateofBirth, string telephone, string direction, string numberWorks, string firstPublication)
+        {
+            this.Login = Login;
+            this.Password = Password;
+            this.Role = Role;
+            this.Email = Email;
+            this.Name = name;
+            this.LastName = lastName;
+            this.DateofBirth = dateofBirth;
+            this.Telephone = telephone;
+            this.Direction = direction;
+            this.NumberWorks = numberWorks;
+            this.FirstPublication = firstPublication;
+        }
+
+    }
+}
